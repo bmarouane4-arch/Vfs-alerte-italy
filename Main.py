df10cc2a8e234a8d657baeb1c4c99873365c3b6f
bmarouane4-arch <bmarouane4@gmail.com>
2026-05-01 14:28:50 +0100

    Update Main.py with hardcoded token and chat ID
    Replace environment variables with hardcoded values and modify appointment check logic.

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-import requests
+8783362495import requests
 import time
-import os
+import random
 
-TOKEN = os.getenv("8783362495:AAHqDon_R9bg5MEYW7yaFN3hyesn9sLi9Qw")
-CHAT_ID = os.getenv("918455655")
+TOKEN = "8783362495:AAHqDon_R9bg5MEYW7yaFN3hyesn9sLi9Qw"
+CHAT_ID = "918455655"
 
 URL = "https://visa.vfsglobal.com/dza/fr/ita/book-an-appointment"
 
@@ -18,12 +18,13 @@ def check_vfs():
 
     r = requests.get(URL, headers=headers)
 
-    if True:
-        send_telegram("🚨 RDV Italie disponible (Constantine / Annaba) !")
+    # إذا لقى موعد
+    if "No appointments available" not in r.text:
+        send_telegram("🚨 يوجد موعد VFS إيطاليا متوفر! احجز بسرعة!")
 
 while True:
     try:
         check_vfs()
-        time.sleep(60)  # كل دقيقة
+        time.sleep(random.randint(30, 60))
     except:
-        time.sleep(120)
+        time.sleep(60)
