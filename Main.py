dc6992f9179048741832fae73316e3015935f029
bmarouane4-arch <bmarouane4@gmail.com>
2026-05-01 14:04:01 +0100

    Update TOKEN and CHAT_ID to hardcoded values
    Replaced environment variables for TOKEN and CHAT_ID with hardcoded values.

---
+++ Main.py
@@ -0,0 +1,29 @@
+import requests
+import time
+import os
+
+TOKEN = os.getenv("8783362495:AAHqDon_R9bg5MEYW7yaFN3hyesn9sLi9Qw")
+CHAT_ID = os.getenv("1777639751")
+
+URL = "https://visa.vfsglobal.com/dza/fr/ita/book-an-appointment"
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+def check_vfs():
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(URL, headers=headers)
+
+    if "No appointments available" not in r.text:
+        send_telegram("🚨 RDV Italie disponible (Constantine / Annaba) !")
+
+while True:
+    try:
+        check_vfs()
+        time.sleep(60)  # كل دقيقة
+    except:
+        time.sleep(120)
