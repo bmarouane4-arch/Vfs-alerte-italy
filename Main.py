aef1ca02f4463986206d503f877a14cd2e06e2fe
bmarouane4-arch <bmarouane4@gmail.com>
2026-05-01 14:53:24 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,33 +1,15 @@
 import requests
 import time
-import random
 
 TOKEN = "8783362495:AAFNNvuZWysgDklNH9UiHK2nqJBzDG6B6P8"
 CHAT_ID = "1202717318"
 
-URL = "https://visa.vfsglobal.com/dza/fr/ita/book-an-appointment"
-
-def send_telegram(msg):
+def send(msg):
     requests.post(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
         data={"chat_id": CHAT_ID, "text": msg}
     )
 
-def check_vfs():
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9"
-    }
-
-    r = requests.get(URL, headers=headers)
-
-    # ❗ ملاحظة: هذا الشرط قد لا يعمل بسبب JavaScript
-    if "No appointments available" not in r.text:
-        send_telegram("🚨 موعد VFS إيطاليا متوفر! احجز بسرعة!")
-
 while True:
-    try:
-        check_vfs()
-        time.sleep(random.randint(30, 60))
-    except:
-        time.sleep(60)
+    send("✅ البوت شغال الآن")
+    time.sleep(60)
